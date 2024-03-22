eff6c40ea53c578b9ffb6ca5d19ce8f30daffe9e
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2024-03-22 12:57:45 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# Define a Puppet class to manage the installation of Flask
-class install_flask {
-    
-    # Execute pip3 command to install Flask version 2.1.0
-    exec { 'install_flask':
-        command => '/usr/bin/pip3 install flask==2.1.0',
-        unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-    }
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-# Include the class to apply its configuration
-include install_flask
